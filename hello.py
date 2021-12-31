5965e7da22d469865d4fed7c56845a5638d11388
Rafael Ferreira <rafael.ferreira@deway.com.br>
2021-12-31 11:41:30 -0300

    routing

--- hello.py
+++ hello.py
@@ -3,10 +3,10 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    return 'Index Page'
 
-@app.route("/<name>")
-def hello(name):
-    return f"Hello, {escape(name)}!"
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
